8648a3f03dd5f74549326bafc7d558f2559384d4
gecosocarlota <carlotagecoso787@gmail.com>
2021-05-07 20:00:45 +0800

    Allowing the users to send email if all the fields are filled

--- js/app.js
+++ js/app.js
@@ -1,52 +1,63 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
         email = document.getElementById('email'),
         subject = document.getElementById('subject'),
-        message = document.getElementById('message');
-        reset = document.getElementById('resetbtn'),	
+        message = document.getElementById('message'),
+        resetBtn = document.getElementById('resetBtn'),
+        sendEmailForm = document.getElementById('email-form');
 
+//Event Listeners
 
-
-// Event Listeners
 eventListeners();
 
-
-function eventListeners(){
-// App init
+function eventListeners() {
+    //App Init
     document.addEventListener('DOMContentLoaded', appInit);
-}
 
-// Validate the forms
+    //Validate the form
     email.addEventListener('blur', validateField);
     subject.addEventListener('blur', validateField);
     message.addEventListener('blur', validateField);
 
+    //Send email and reset button
+    resetBtn.addEventListener('click', resetForm);
+}
 
+//Functions
 
-// Functions
-
-// App intitialization
+//Aapp Initialization
 function appInit() {
-// disable the send button on load
+    //disable the send button on load
     sendBtn.disabled = true;
 }
 
-// Validate the form fields
+//Validate the fields
 function validateField() {
     let errors;
 
+    //Validate the length of the field
+    validateLength(this);
 
-    // Validate the length of the field
-     validateLength(this);
-
-    // Validate email
-    if(this.type === 'email') {
+    //Validate the email
+    if (this.type === 'email') {
         validateEmail(this);
     }
+
+    //Both will return errors, then check if there are any errors
+    errors = document.querySelectorAll('.error');
+
+    //Check that the inputs are not empty 
+    if (email.value !== '' && subject.value !== '' && message.value !== '' ) {
+        if (errors.length === 0) {
+            //the button should be enabled
+            sendBtn.disabled = false;
+        }
+    }
 }
-// Validate the length of the fields
+
+//Validate of the length of the fields
 function validateLength(field) {
-    if(field.value.length > 0 ) {
+    if (field.value.length > 0) {
         field.style.borderBottomColor = 'green';
         field.classList.remove('error');
     } else {
@@ -54,16 +65,22 @@ function validateLength(field) {
         field.classList.add('error');
     }
 }
-// Validate email (checks for @ in the value)
+
+//Validate email (checks for @ in the value)
 function validateEmail(field) {
     let emailText = field.value;
-        // check if the emailText contains the @ sign
-        if(emailText.indexOf('@') !== -1) {
-            field.style.borderBottomColor = 'green';
-            field.classList.remove('error');
-        } else {
-            field.style.borderBottomColor = 'red';
-            field.classList.add('error');
-        }
+
+    //check if the emailText contains the @ sign
+    if (emailText.indexOf('@') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
 
+//Reset the form
+function resetForm() {
+    sendEmailForm.reset();
+}
