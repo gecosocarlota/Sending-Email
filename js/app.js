b0bb1437a684c01a8191e4455595f8af61200af9
gecosocarlota <carlotagecoso787@gmail.com>
2021-05-07 19:59:07 +0800

    Validating Fields Part 2

--- js/app.js
+++ js/app.js
@@ -1,35 +1,69 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn'),
+const sendBtn = document.getElementById('sendBtn');
         email = document.getElementById('email'),
         subject = document.getElementById('subject'),
         message = document.getElementById('message');
+        reset = document.getElementById('resetbtn'),	
 
-//Event Listeners
 
+
+// Event Listeners
 eventListeners();
 
-function eventListeners() {
-    //App Init
-    document.addEventListener('DOMContentLoaded', appInit);
 
-    //Validate the form
+function eventListeners(){
+// App init
+    document.addEventListener('DOMContentLoaded', appInit);
+}
+
+// Validate the forms
     email.addEventListener('blur', validateField);
     subject.addEventListener('blur', validateField);
     message.addEventListener('blur', validateField);
-}
 
-//Functions
 
-//Aapp Initialization
+
+// Functions
+
+// App intitialization
 function appInit() {
-    //disable the send button on load
+// disable the send button on load
     sendBtn.disabled = true;
 }
 
-//Validate the fields
+// Validate the form fields
 function validateField() {
     let errors;
 
-    //Validate the length of the field
-    validateLength(this)
+
+    // Validate the length of the field
+     validateLength(this);
+
+    // Validate email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
 }
+// Validate the length of the fields
+function validateLength(field) {
+    if(field.value.length > 0 ) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+// Validate email (checks for @ in the value)
+function validateEmail(field) {
+    let emailText = field.value;
+        // check if the emailText contains the @ sign
+        if(emailText.indexOf('@') !== -1) {
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+        } else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+        }
+}
+
